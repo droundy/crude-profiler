8f3c3fdd2595081849ac1145aeb6fa7f9c4c5527
David Roundy <roundyd@physics.oregonstate.edu>
2017-06-19 11:53:49 -0700

    make things a bit prettier.

--- src/lib.rs
+++ src/lib.rs
@@ -47,6 +47,7 @@ impl Drop for Guard {
         let mut m = PROFILE.lock().unwrap();
         m.add_time(now);
         m.stack.pop();
+        println!("drop");
     }
 }
 
@@ -58,6 +59,21 @@ pub fn push(task: &'static str) -> Guard {
     Guard {}
 }
 
+pub fn replace(task: &'static str) -> Guard {
+    let now = std::time::Instant::now();
+    let mut m = PROFILE.lock().unwrap();
+    m.add_time(now);
+    m.stack = vec![task];
+    Guard {}
+}
+
+pub fn clear() {
+    let mut m = PROFILE.lock().unwrap();
+    m.times = HashMap::new();
+    m.stack = Vec::new();
+    m.started = std::time::Instant::now();
+}
+
 fn pretty_stack(v: &Vec<&'static str>) -> String {
     let mut out = String::new();
     for s in v {
@@ -67,12 +83,20 @@ fn pretty_stack(v: &Vec<&'static str>) -> String {
     out
 }
 
+fn duration_to_f64(t: std::time::Duration) -> f64 {
+    t.as_secs() as f64 + (t.subsec_nanos() as f64)*1e-9
+}
+
 pub fn report() -> String {
-    let m = PROFILE.lock().unwrap();
+    let now = std::time::Instant::now();
+    let mut m = PROFILE.lock().unwrap();
+    m.add_time(now);
     let mut out = String::new();
-    for (k,v) in m.times.iter() {
+    let mut keys: Vec<_> = m.times.keys().collect();
+    keys.sort();
+    for k in keys {
         out.push_str(&pretty_stack(k));
-        out.push_str(&format!(" {:?}\n", v));
+        out.push_str(&format!(" {}\n", duration_to_f64(m.times[k])));
     }
     out
 }
@@ -82,9 +106,20 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
+        clear();
         push("hello world");
         let rep = report();
         println!("{}", rep);
         assert!(rep.contains("hello world"));
+    }
+    #[test]
+
+    fn nesting() {
+        clear();
+        let _a = push("hello");
+        let _b = push("world");
+        let rep = report();
+        println!("{}", rep);
+        assert!(rep.contains("hello:world"));
     }
 }
